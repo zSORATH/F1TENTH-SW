01db45b82b0d2fc6ccb31dc66818286d4d603436
zSORATH <60431537+zSORATH@users.noreply.github.com>
2022-05-17 12:25:11 +0200

    Add files via upload

---
+++ RabbitMQ/lidarMQ.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+#for ros
+#from requests import request
+import rospy
+
+#for rabbitmq
+import pika
+import datetime
+import json
+
+from sensor_msgs.msg import LaserScan
+
+#import numpy as np
+
+class LidarMQ():
+    def __init__(self):
+        
+        rospy.init_node('LidarMQ', anonymous=True)
+        self.rate = rospy.Rate(100) # 10hz
+
+        self.ranges = 0   
+        
+        #Connection to broker
+        self.connectionToBroker = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        self.connectionFromBroker = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+
+        #Channel to broker
+        self.channelToBroker = self.connectionToBroker.channel()
+        self.channelToBroker.exchange_declare(exchange='topic_logs', exchange_type='topic')
+
+        #Subscribing to LaserScan
+        self.sub=rospy.Subscriber('scan', LaserScan, self.lidar_callback)       
+
+        
+        
+        
+
+    def lidar_callback(self, scan):  
+
+        #get time with for rabbit msg
+        rt = rospy.get_rostime()
+        rostime = rt.secs + rt.nsecs * 1e-09
+        rostimeISO = datetime.datetime.strptime(datetime.datetime.utcfromtimestamp(rostime).isoformat(timespec='milliseconds')+'+0100', "%Y-%m-%dT%H:%M:%S.%f%z")
+        
+        #checking if the send channel and connection is open
+        if not self.channelToBroker or self.connectionToBroker.is_closed:
+            self.channelToBroker = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+            self.channelToBroker = self.connectionToBroker.channel()
+            self.channelToBroker.exchange_declare(exchange='topic_logs', exchange_type='topic')
+        
+        #Getting ranges from LaserScan
+        self.ranges = scan.ranges
+       
+        # CHANGE ROUTING KEY
+        routing_key = "lidar.ranges"
+        message = {
+            'time': rostimeISO.isoformat(timespec='milliseconds'),         
+            'ranges': self.ranges
+            }
+            
+        self.channelToBroker.basic_publish(
+            exchange='topic_logs', routing_key=routing_key, body=json.dumps(message))
+
+        
+if __name__ == '__main__': 
+    try: 
+        LidarMQ()
+        print("Spinning")
+        rospy.spin()
+
+    except rospy.ROSInterruptException:
+        pass
